6afc384eb839a119742701fb7b6c5ba73de45d5e
Muhammad Zikrullaev <zikrullaevmuhammad@mail.ru>
2022-08-01 16:02:54 +0300

    commit project

---
+++ src/main/java/com/mzik/spring/rest/dao/EmployeeDAO.java
@@ -0,0 +1,16 @@
+package com.mzik.spring.rest.dao;
+
+import com.mzik.spring.rest.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeDAO {
+
+    public List<Employee> getAllEmployees();
+
+    public void saveEmployee(Employee employee);
+
+    public Employee getEmployee(int id);
+
+    public void deleteEmployee(int id);
+}
